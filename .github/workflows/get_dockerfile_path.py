8e57ca8799b98207a7d7390b0bb64ebb823ebf9d
Edward Gaibor <78832141+gaiborjosue@users.noreply.github.com>
2023-07-17 16:40:04 -0500

    added workflow python files for docker

---
+++ .github/workflows/get_dockerfile_path.py
@@ -0,0 +1,22 @@
+import os
+
+def get_latest_model_name():
+    org_folder = max([folder for folder in os.listdir('.') if os.path.isdir(folder)], key=os.path.getctime)
+    model_folder = max([folder for folder in os.listdir(org_folder) if os.path.isdir(os.path.join(org_folder, folder))], key=os.path.getctime)
+    return os.path.join(org_folder, model_folder)
+
+def get_dockerfile_path(model_folder):
+    for root, _, files in os.walk(model_folder):
+        for file in files:
+            if file.lower() == "dockerfile":
+                return os.path.join(root)
+    return None
+
+if __name__ == "__main__":
+    model_folder = get_latest_model_name()
+
+    dockerfile_path = get_dockerfile_path(model_folder)
+    if dockerfile_path:
+        print(dockerfile_path)
+    else:
+        print("Dockerfile not found.")
