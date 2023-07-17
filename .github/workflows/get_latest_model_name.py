4392dfebb9345573300c3fef98303f28d387b7ad
Edward Gaibor <78832141+gaiborjosue@users.noreply.github.com>
2023-07-17 17:51:38 -0500

    update_path_model_name

--- .github/workflows/get_latest_model_name.py
+++ .github/workflows/get_latest_model_name.py
@@ -1,6 +1,10 @@
 import os
 
 def get_latest_model_name():
+    path = "./trained-models-fork"
+
+    os.chdir(path)
+
     org_folder = max([folder for folder in os.listdir('.') if os.path.isdir(folder)], key=os.path.getctime)
     model_folder = max([folder for folder in os.listdir(org_folder) if os.path.isdir(os.path.join(org_folder, folder))], key=os.path.getctime)
     return os.path.join(org_folder, model_folder)
