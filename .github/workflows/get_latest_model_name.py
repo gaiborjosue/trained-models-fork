afff1e9005d5c4ff0e2f1a6057a70e2dc331dcba
Edward Gaibor <78832141+gaiborjosue@users.noreply.github.com>
2023-07-17 17:33:44 -0500

    updated_latest_model_name

--- .github/workflows/get_latest_model_name.py
+++ .github/workflows/get_latest_model_name.py
@@ -1,12 +1,7 @@
 import os
 
 def get_latest_model_name():
-    org_folder = max([folder for folder in os.listdir('.') if os.path.isdir(folder)], key=os.path.getctime)
-    model_folder = max([folder for folder in os.listdir(org_folder) if os.path.isdir(os.path.join(org_folder, folder))], key=os.path.getctime)
-    return os.path.basename(model_folder)
-
-if __name__ == "__main__":
-        # Get the script's current file path
+    # Get the script's current file path
     script_path = os.path.abspath(__file__)
 
     # Navigate two folders back to the root of the repository
@@ -17,5 +12,11 @@ if __name__ == "__main__":
 
     # Move two directories back to the desired location
     os.chdir("..")
-    
+
+    org_folder = max([folder for folder in os.listdir('.') if os.path.isdir(folder)], key=os.path.getctime)
+    model_folder = max([folder for folder in os.listdir(org_folder) if os.path.isdir(os.path.join(org_folder, folder))], key=os.path.getctime)
+    return os.path.basename(model_folder)
+
+if __name__ == "__main__":
+
     print(get_latest_model_name())
