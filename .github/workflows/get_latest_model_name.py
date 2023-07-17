ed98ceb1fd66b38d18bc9b382da6506469e46017
Edward Gaibor <78832141+gaiborjosue@users.noreply.github.com>
2023-07-17 17:43:43 -0500

    update

--- .github/workflows/get_latest_model_name.py
+++ .github/workflows/get_latest_model_name.py
@@ -1,9 +1,12 @@
 import os
 
 def get_latest_model_name():
+# Get the root of the repository using the GITHUB_WORKSPACE environment variable
     root_path = os.environ.get('GITHUB_WORKSPACE')
 
-    org_folder = max([folder for folder in os.listdir(root_path) if os.path.isdir(folder)], key=os.path.getctime)
+    os.chdir(root_path)
+
+    org_folder = max([folder for folder in os.listdir('.') if os.path.isdir(folder)], key=os.path.getctime)
     model_folder = max([folder for folder in os.listdir(org_folder) if os.path.isdir(os.path.join(org_folder, folder))], key=os.path.getctime)
     return os.path.basename(model_folder)
 
