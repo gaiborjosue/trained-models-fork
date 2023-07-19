b3f057ecb017fe4b287942649228e4dcee184ac1
Edward Gaibor <78832141+gaiborjosue@users.noreply.github.com>
2023-07-18 20:26:05 -0500

    test

--- .github/workflows/get_latest_model_name.py
+++ .github/workflows/get_latest_model_name.py
@@ -11,26 +11,40 @@ def extract_organization_name(pull_request_description):
         print(f"Error while parsing YAML: {e}")
         return None
 
-def get_latest_model_name():
-    if os.path.exists('./pr_description.yaml'):
+def get_pull_request_description():
+    # Fetch the GitHub Pull Request event payload using GitHub API
+    pr_event_url = os.environ.get('GITHUB_EVENT_PATH', None)
+    if pr_event_url is None:
+        print("Error: GITHUB_EVENT_PATH environment variable not set.")
+        exit()
 
-        with open('./pr_description.yaml', 'r') as file:
-            pull_request_description = file.read()
+    with open(pr_event_url, 'r') as file:
+        pr_event_data = yaml.safe_load(file)
 
-        org_folder = extract_organization_name(pull_request_description)
-        print(org_folder)
-        # Cd into the org folder
-        os.chdir(org_folder)
+    # Extract the pull request description from the payload
+    pull_request_description = pr_event_data.get('pull_request', {}).get('body', None)
 
-        # List all files
-        model_n = os.listdir('.')[0]
-
-        if model_n == 'objects':
-            exit()
-        else:
-            return model_n
+    if pull_request_description is None:
+        print("Error: Pull Request Description not found in the event payload.")
+        exit()
     else:
-        return "pr_description.yaml not found."
+        return pull_request_description
+    
+def get_latest_model_name():
+    pull_request_description = get_pull_request_description()
+
+    org_folder = extract_organization_name(pull_request_description)
+    print(org_folder)
+    # Cd into the org folder
+    os.chdir(org_folder)
+
+    # List all files
+    model_n = os.listdir('.')[0]
+
+    if model_n == 'objects':
+        exit()
+    else:
+        return model_n
 
 if __name__ == "__main__":
 
